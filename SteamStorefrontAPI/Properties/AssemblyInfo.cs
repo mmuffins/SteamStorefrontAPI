71b62ff30ad6fa77ecb359e111e0558be89dea0e
mmuffins <mail@tiv73.com>
2018-05-24 22:19:21 +0200

    Updated assembly information.

--- SteamStorefrontAPI/Properties/AssemblyInfo.cs
+++ SteamStorefrontAPI/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SteamStorefrontAPI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A .NET wrapper for the Steam Storefront API")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SteamStorefrontAPI")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © mmuffins 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
