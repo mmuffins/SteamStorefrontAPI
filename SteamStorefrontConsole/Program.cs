1d3232ceb846c575dd0649c7aa2f556abb437e3e
mmuffins <mail@tiv73.com>
2018-05-24 22:16:39 +0200

    Renamed GetGame function to a more logical name.

--- SteamStorefrontConsole/Program.cs
+++ SteamStorefrontConsole/Program.cs
@@ -13,27 +13,23 @@ namespace SteamStorefrontConsole
     {
         static void Main(string[] args)
         {
-            Task.Run(async () => await GetGame()).Wait();
-
+            Task.Run(async () => await Examples()).Wait();
         }
 
-        static async Task GetGame()
+        static async Task Examples()
         {
             var steamApp1 = await AppDetails.GetAsync(637670);
             var steamApp2 = await AppDetails.GetAsync(443790);
             var steamApp3 = await AppDetails.GetAsync(460810, "JP");
-            var steamApp4 = await AppDetails.GetAsync(322330);
-            var steamApp5 = await AppDetails.GetAsync(241930);
+            var steamApp4 = await AppDetails.GetAsync(322330, "US");
 
-            //var package1 = await PackageDetails.GetAsync(68179);
-            //var package2 = await PackageDetails.GetAsync(68179, "JP");
-            //var package3 = await PackageDetails.GetAsync(235158);
-            //var package4 = await PackageDetails.GetAsync(235158, "US");
+            var package1 = await PackageDetails.GetAsync(68179);
+            var package2 = await PackageDetails.GetAsync(68179, "JP");
+            var package3 = await PackageDetails.GetAsync(235158);
+            var package4 = await PackageDetails.GetAsync(235158, "US");
 
-            //var featured = await Featured.GetAsync();
-            //var featuredCategories = await FeaturedCategories.GetAsync();
-
-            //Console.WriteLine(featured);
+            var featured = await Featured.GetAsync();
+            var featuredCategories = await FeaturedCategories.GetAsync();
         }
     }
 }
