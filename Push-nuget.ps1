f7b26a4055f9b3608ce7be81ff515687b533c43c
mmuffins <mail@tiv73.com>
2018-05-27 18:52:08 +0200

    Updated nuget script.

--- Push-nuget.ps1
+++ Push-nuget.ps1
@@ -1,4 +1,5 @@
 $apiKey = Read-Host "Please enter your nuget api key"
+Get-ChildItem -File -Filter SteamStorefrontAPI.*nupkg | Remove-Item -Force
 nuget pack .\SteamStorefrontAPI\SteamStorefrontAPI.csproj -Symbols -Prop Configuration=Release
 nuget push SteamStorefrontAPI.*.nupkg -ApiKey $apiKey -source nuget.org
 nuget push SteamStorefrontAPI.*.symbols.nupkg -ApiKey $apiKey -source https://nuget.smbsrc.net/
