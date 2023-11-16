7c89c16f34a92e38d83cce105e9ede7e5a2a9eed
mmuffins <mail@tiv73.com>
2023-11-16 22:05:20 +0100

    Update unit tests

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -5,13 +5,27 @@ namespace Tests
 {
     public class UnitTests
     {
-        [Theory]
-        [InlineData(550)]
-        [InlineData(620)]
-        public async Task GetAppDetails(int appId)
+        public class AppDetailsTestObject
         {
-            SteamApp appDetails = await AppDetails.GetAsync(appId);
-            Assert.Equal(appId, appDetails.SteamAppId);
+            public int AppId { get; set; }
+            public string Name { get; set; }
+            public string AdditionalProperty1 { get; set; }
+        }
+
+        public static IEnumerable<object[]> AppDetailsData()
+        {
+            yield return new object[] { new AppDetailsTestObject { AppId = 550, Name = "Left 4 Dead 2", AdditionalProperty1 = "Value1" } };
+            yield return new object[] { new AppDetailsTestObject { AppId = 620, Name = "Portal 2", AdditionalProperty1 = "Value2" } };
+            yield return new object[] { new AppDetailsTestObject { AppId = 546560, Name = "Half-Life: Alyx", AdditionalProperty1 = "Value2" } };
+        }
+
+        [Theory]
+        [MemberData(nameof(AppDetailsData))]
+        public async Task GetAppDetails(AppDetailsTestObject testData)
+        {
+            SteamApp appDetails = await AppDetails.GetAsync(testData.AppId);
+            Assert.Equal(testData.AppId, appDetails.SteamAppId);
+            Assert.Equal(testData.Name, appDetails.Name);
         }
     }
 }
